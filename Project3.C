dc84df9a40c0166716be5b8aadcc4af8c5bf3021
ChishaNa777 <nathanchisha11@gmail.com>
2025-10-01 13:22:51 +0300

    Project3.C

---
+++ Project3.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A=16,B=8; //declaring two variables
+    int Sum,Difference,Product,Quotient;
+    Sum=A+B; Difference=A-B; Product=A*B; Quotient=A/B;
+    printf("\nSum=%d,\nDifference=%d,\nProduct=%d,\nQuotience%d",Sum,Difference,Product,Quotient);
+    // printing the sum,difference,product and quotient of two numbers
+    return 0;
+}
